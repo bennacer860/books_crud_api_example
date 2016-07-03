4461bb40f4aeb1c59c4701defff42638c45537ed
bennacer860 <bennacer860@gmail.com>
2016-07-03 18:35:22 -0400

    deal with diffent format

---
+++ app/controllers/finished_books_controller.rb
@@ -0,0 +1,9 @@
+class FinishedBooksController < ApplicationController
+  def index
+    books = Book.all
+    respond_to  do |format|
+      format.json { render json: books , status: 200  }
+      format.xml { render xml: books , status: 200  }
+    end
+  end
+end
