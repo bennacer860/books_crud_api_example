a138aa559a9bf1e18363a896f60801f060a840cd
bennacer860 <bennacer860@gmail.com>
2016-07-03 18:31:25 -0400

    content negociation test

--- test/integration/listing_finished_books_test.rb
+++ test/integration/listing_finished_books_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ListingFinishedBooksTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'list finished books in json' do
+    get '/finished_books' ,{},{'Accept' => 'application/json'}
+    assert_equal 200,response.status
+    assert_equal Mime::JSON, response.content_type
+  end
+
+  test 'list finished books in xml' do
+    get '/finished_books' ,{},{'Accept' => 'application/xml'}
+    assert_equal 200,response.status
+    assert_equal Mime::JSON, response.content_type
+  end
+
 end
