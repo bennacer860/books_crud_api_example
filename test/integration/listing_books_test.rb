351f5743d1215edff88c8d1765856e4b00edec4d
bennacer860 <bennacer860@gmail.com>
2016-07-03 18:11:06 -0400

    add book model

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -10,7 +10,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     get '/books'
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    assert_equal Book.count, JSON.prase(response.body).size
+    assert_equal Book.count, JSON.parse(response.body).size
   end
 
 end
