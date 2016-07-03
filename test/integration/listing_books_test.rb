5c49374354d0c9f76021a8bb983d9aa87899c834
bennacer860 <bennacer860@gmail.com>
2016-07-03 18:08:45 -0400

    add assertions

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
+  setup do
+    Book.create(title: "Game of Thrones", rating: 5)
+    Book.create(title: "Game of Thrones : Ice and Fire", rating: 4)
+  end
+  
   test 'listing books' do
     get '/books'
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
+    assert_equal Book.count, JSON.prase(response.body).size
   end
 
 end
