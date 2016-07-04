9ea3c8ef1495f700c71db02ffd167e5c2a6a1965
bennacer860 <bennacer860@gmail.com>
2016-07-04 10:12:54 -0400

    pass test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,4 +16,18 @@ class BooksController < ApplicationController
     end
     render json: books, status: 200
   end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: 201, location: book
+    else
+
+    end
+  end
+
+
+  def book_params
+    params.require(:book).permit(:title,:rating)
+  end
 end
--- test/integration/creating_books_test.rb
+++ test/integration/creating_books_test.rb
@@ -3,10 +3,12 @@ require 'test_helper'
 class CreatingBooksTest < ActionDispatch::IntegrationTest
 
   test 'create a new book' do
-    post '/books', {} , {}
-    assert_equal 201,reponse.status
+    post '/books', { book: {title: "title",rating: 5} }.to_json , { "Accept" => "application/json", "Content-Type" => "application/json" }
+    assert_equal 201,response.status
     book = json(response.body)
     assert_equal book_url(book[:id]), response.location
     assert_equal Mime::JSON, response.content_type
+    assert_equal "title",book[:title]
+    assert_equal 5,book[:rating]
   end
 end
