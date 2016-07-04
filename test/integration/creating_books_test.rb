169bcf95b4a10471ffb4a2de7ebe0d2041cb979a
bennacer860 <bennacer860@gmail.com>
2016-07-04 10:17:25 -0400

    crereate a test create book with invalid data

--- test/integration/creating_books_test.rb
+++ test/integration/creating_books_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CreatingBooksTest < ActionDispatch::IntegrationTest
 
-  test 'create a new book' do
+  test 'create a new book with valid data' do
     post '/books', { book: {title: "title",rating: 5} }.to_json , { "Accept" => "application/json", "Content-Type" => "application/json" }
     assert_equal 201,response.status
     book = json(response.body)
@@ -11,4 +11,10 @@ class CreatingBooksTest < ActionDispatch::IntegrationTest
     assert_equal "title",book[:title]
     assert_equal 5,book[:rating]
   end
+
+  test 'not create book with invalid data ' do
+    post '/books', { book: {title: nil,rating: 5} }.to_json , { "Accept" => "application/json", "Content-Type" => "application/json" }
+    assert_equal 422,response.status
+  end
+
 end
