8d2b2b228344d47057392d982d810419a3586487
bennacer860 <bennacer860@gmail.com>
2016-07-04 22:36:41 -0400

    change namespace

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::BooksController < ApplicationController
+  # skip_before_action :verify_authenticity_token, if: :json_request?
+  protect_from_forgery with: :null_session, if: Proc.new {|c| c.request.format.json? }
+
+
+  def json_request?
+    request.format.json?
+  end
+
+  def index
+    books = []
+    if rating = params[:rating]
+      books = Book.where(rating: rating)
+    else
+      books = Book.all
+    end
+    render json: books, status: 200
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: 201, location: api_v1_book_url(book)
+    else
+      render json: book.errors, status: 422
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy!
+    render nothing: true, status: 204
+  end
+
+  def book_params
+    params.require(:book).permit(:title,:rating)
+  end
+end
---
+++ app/controllers/api/v1/finished_books_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::FinishedBooksController < ApplicationController
+  def index
+    books = Book.finished
+    respond_to  do |format|
+      format.json { render json: books , status: 200  }
+      format.xml { render xml: books , status: 200  }
+    end
+  end
+end
